0770dd7abda2bca84909cabb07f19ee8fd1a3f74
KitijaC <cietvirakitija@gmail.com>
2023-02-28 18:16:55 +0200

    init

--- App.jsx
+++ App.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
+import React from "react"
 import Die from "./components/Die"
 import {nanoid} from "nanoid"
 import Confetti from "react-confetti"
 
 export default function App() {
-   
+
     const [dice, setDice] = React.useState(allNewDice())
     const [tenzies, setTenzies] = React.useState(false)
-
+    
     React.useEffect(() => {
-        const allHeld = dice.every(die => die.isHeld) 
+        const allHeld = dice.every(die => die.isHeld)
         const firstValue = dice[0].value
         const allSameValue = dice.every(die => die.value === firstValue)
         if (allHeld && allSameValue) {
             setTenzies(true)
         }
     }, [dice])
-
 
     function generateNewDie() {
         return {
@@ -34,7 +33,7 @@ export default function App() {
         return newDice
     }
     
-     function rollDice() {
+    function rollDice() {
         if(!tenzies) {
             setDice(oldDice => oldDice.map(die => {
                 return die.isHeld ? 
@@ -47,15 +46,29 @@ export default function App() {
         }
     }
     
+    function holdDice(id) {
+        setDice(oldDice => oldDice.map(die => {
+            return die.id === id ? 
+                {...die, isHeld: !die.isHeld} :
+                die
+        }))
+    }
+    
     const diceElements = dice.map(die => (
-        <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} /> 
+        <Die 
+            key={die.id} 
+            value={die.value} 
+            isHeld={die.isHeld} 
+            holdDice={() => holdDice(die.id)}
+        />
     ))
-
+    
     return (
         <main>
             {tenzies && <Confetti />}
             <h1 className="title">Tenzies</h1>
-            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+            <p className="instructions">Roll until all dice are the same. 
+            Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
                 {diceElements}
             </div>
